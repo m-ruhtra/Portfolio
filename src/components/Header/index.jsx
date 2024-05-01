b2402f53df86cc2c90f3836b4ca7645efe6b627f
m-ruhtra <arthur.mig.rodrigues@gmail.com>
2024-04-30 21:20:11 -0300

    feat: Implementação do componente Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+export const Header = () => {
+    return(
+        <header>
+            <h1>Arthur Miguel Rodrigues</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Tecnologias</a></li>
+                    <li><a href="#">Projetos</a></li>
+                    <li><a href="#">Contatos</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+};
