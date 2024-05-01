3e7a9a309e9d243b4434224fe9e395bfee4593f1
m-ruhtra <arthur.mig.rodrigues@gmail.com>
2024-04-30 22:00:09 -0300

    feat: Data para listagem das Techs

---
+++ src/data/techsData.js
@@ -0,0 +1,27 @@
+import iconHtml from "../assets/icons/iconHtml.svg";
+import iconCss from "../assets/icons/iconCss.svg";
+import iconJavascript from "../assets/icons/iconJavascript.svg";
+import iconReact from "../assets/icons/iconReact.svg";
+
+export const dataStacks = [
+    {
+        id: 1,
+        name: "HTML5",
+        img: iconHtml,
+    },
+    {
+        id: 2,
+        name: "CSS3",
+        img: iconCss,
+    },
+    {
+        id: 3,
+        name: "JavaScript",
+        img: iconJavascript,
+    },
+    {
+        id: 4,
+        name: "ReactJS",
+        img: iconReact,
+    },
+];
