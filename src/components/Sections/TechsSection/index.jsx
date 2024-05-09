70c359963cf4722d21400073748eb92148e26fb7
m-ruhtra <arthur.mig.rodrigues@gmail.com>
2024-05-08 22:21:10 -0300

    style: Alteração de cor dos cards de projetos e rolagem de página

--- src/components/Sections/TechsSection/index.jsx
+++ src/components/Sections/TechsSection/index.jsx
@@ -3,7 +3,7 @@ import styles from "./style.module.css";
 
 export const TechSection = () => {
     return(
-        <section id="techsSection" className="container">
+        <section id="techsSection" className="container_section active">
             <div className={styles.section_box}>
                 <h2 className="title sm">Habilidades</h2>
 
